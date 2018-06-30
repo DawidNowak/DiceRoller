d1597d9a3f4722aad74dd5b8804167e8e5182aaf
Dawid Nowak <dnowak3@pl.sii.eu>
2018-06-30 07:27:13 +0200

    CameraHelper fixed image rotating from landscape to portrait

--- DiceRoller/DiceRoller/Helpers/CameraHelper.cs
+++ DiceRoller/DiceRoller/Helpers/CameraHelper.cs
@@ -12,42 +12,51 @@ using MediaOrientation = Android.Media.Orientation;
 
 namespace DiceRoller.Helpers
 {
-    public static class CameraHelper
-    {
-	    public static async Task<byte[]> TakePicture()
-	    {
-		    byte[] byteArr;
+	public static class CameraHelper
+	{
+		public static async Task<byte[]> TakePicture()
+		{
 			await CrossMedia.Current.Initialize();
 
-		    if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
-		    {
-			    //View.DisplayAlert("No Camera", "No camera available.", "OK");
-			    return null;
-		    }
+			if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+			{
+				//View.DisplayAlert("No Camera", "No camera available.", "OK");
+				return null;
+			}
 
 			var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
-		    {
-			    AllowCropping = true,
-			    DefaultCamera = CameraDevice.Front,
-			    SaveToAlbum = false,
-			    Name = "test.jpg",
+			{
+				AllowCropping = true,
+				DefaultCamera = CameraDevice.Front,
+				SaveToAlbum = false,
+				Name = "test.jpg",
 				RotateImage = false,
 				CustomPhotoSize = 50,
 				CompressionQuality = 80
-		    });
+			});
 
-		    if (file != null)
-		    {
-				using (var memStream = new MemoryStream())
-			    {
-				    file.GetStream().CopyTo(memStream);
-					file.Dispose();
-				    byteArr = memStream.ToArray();
-			    }
-		    }
-		    else byteArr = new byte[0];
+			return file != null ? FlipToPortrait(file) : new byte[0];
+		}
 
-			return byteArr;
-	    }
+		private static byte[] FlipToPortrait(MediaFile file)
+		{
+			var options = new BitmapFactory.Options { InJustDecodeBounds = false };
+			var bitmap = BitmapFactory.DecodeFile(file.Path, options);
+
+			var mtx = new Matrix();
+			mtx.PreRotate(90);
+			bitmap = Bitmap.CreateBitmap(bitmap, 0, 0, bitmap.Width, bitmap.Height, mtx, false);
+			mtx.Dispose();
+			mtx = null;
+
+			byte[] bitmapData;
+			using (var stream = new MemoryStream())
+			{
+				bitmap.Compress(Bitmap.CompressFormat.Jpeg, 0, stream);
+				bitmapData = stream.ToArray();
+			}
+
+			return bitmapData;
+		}
 	}
 }
