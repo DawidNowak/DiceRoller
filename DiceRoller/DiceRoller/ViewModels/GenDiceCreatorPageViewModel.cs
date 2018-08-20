e4d2614fa345042a363e477589b6ad6c1875b7d8
Dawid Nowak <dnowak3@pl.sii.eu>
2018-08-20 09:48:50 +0200

    generated dice creation fix

--- DiceRoller/DiceRoller/ViewModels/GenDiceCreatorPageViewModel.cs
+++ DiceRoller/DiceRoller/ViewModels/GenDiceCreatorPageViewModel.cs
@@ -7,62 +7,63 @@ using Prism.Navigation;
 namespace DiceRoller.ViewModels
 {
 	public class GenDiceCreatorPageViewModel : SaveableBaseViewModel<Dice>
-    {
+	{
 		private readonly IContext _ctx;
-	    public GenDiceCreatorPageViewModel(INavigationService navigationService, IContext ctx) : base(navigationService)
-	    {
+		public GenDiceCreatorPageViewModel(INavigationService navigationService, IContext ctx) : base(navigationService)
+		{
 			_ctx = ctx;
 		}
 
-	    public Action RefreshGame { get; set; }
+		public Action RefreshGame { get; set; }
 
 		private string _path;
 
-	    public string Path
-	    {
-		    get => _path;
-		    set => SetProperty(ref _path, value);
-	    }
+		public string Path
+		{
+			get => _path;
+			set => SetProperty(ref _path, value);
+		}
 
-	    private int _wallsCount;
-	    public int WallsCount
-	    {
-		    get => _wallsCount;
-		    set
-		    {
-			    SetProperty(ref _wallsCount, value);
-			    SaveCommand.RaiseCanExecuteChanged();
-		    }
-	    }
+		private int _wallsCount;
+		public int WallsCount
+		{
+			get => _wallsCount;
+			set
+			{
+				SetProperty(ref _wallsCount, value);
+				SaveCommand.RaiseCanExecuteChanged();
+			}
+		}
 
 		private int _startValue;
-	    public int StartValue
-	    {
-		    get => _startValue;
-		    set => SetProperty(ref _startValue, value);
-	    }
+		public int StartValue
+		{
+			get => _startValue;
+			set => SetProperty(ref _startValue, value);
+		}
 
 		public override void SetModel(Dice model)
 		{
 			base.SetModel(model);
-			var diceData = new DiceData(model.Path == "Generated"? "d0_0." : model.Path);
+			var diceData = new DiceData(model.Path == "Generated" ? "d0_0." : model.Path);
 			Path = model.Path;
 			WallsCount = diceData.WallsCount;
 			StartValue = diceData.StartValue;
 		}
 
 		protected override async void save()
-	    {
-		    Model.Path = $"d{WallsCount}_{StartValue}.";
-		    Model.MiniImage = DrawHelper.DrawDice(new DiceData(Model.Path), false).ToArray();
+		{
+			Model.Path = $"d{WallsCount}_{StartValue}.";
+			Model.MiniImage = DrawHelper.DrawDice(new DiceData(Model.Path), false).ToArray();
 			RefreshGame?.Invoke();
-		    await App.MasterDetail.Detail.Navigation.PopAsync();
+			await App.MasterDetail.Detail.Navigation.PopAsync();
 		}
 
-	    protected override bool canSave()
-	    {
+		protected override bool canSave()
+		{
+			if (Model == null) return false;
 			Model.IsValid = WallsCount > 1;
 			return Model.IsValid;
-	    }
-    }
+		}
+	}
 }
